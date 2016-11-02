703b4cdd26083fe6d3c6f8c68589914ab89c260f
CJButton <gsyme1907@gmail.com>
2016-11-01 19:17:07 -0700

    Start on React Router and Session Components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+
+
+
+import React from 'react';
+
+const App = ({ children }) => (
+  <div>
+    <h1>Bench BnB</h1>
+    {children}
+  </div>
+);
+
+export default App;
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,12 @@
+
+
+
+import { combineReducers } from 'redux';
+
+import SessionReducer from './session_reducer';
+
+const RootReducer = combineReducers({
+  session: SessionReducer
+});
+
+export default RootReducer;
