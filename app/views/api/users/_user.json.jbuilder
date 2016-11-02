7a247f76ad5e648d95eb16069a922809eaaf0918
CJButton <gsyme1907@gmail.com>
2016-11-01 17:32:59 -0700

    finish making new users

---
+++ app/views/api/users/_user.json.jbuilder
@@ -0,0 +1,5 @@
+
+
+
+json.extract! user, :id, :username
+json.favorite_benches user.favorite_benches.pluck(:id)
--- frontend/mangareads.jsx
+++ frontend/mangareads.jsx
@@ -2,8 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {login} from 'util/session_api_util.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>こんにちは</h1>, root);
+    window.login = login;
 });
