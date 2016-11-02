646c68fa9190d8ab82dbfc740eef999999386083
CJButton <gsyme1907@gmail.com>
2016-11-01 22:19:43 -0700

    Continue from bootstrapping auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,10 +2,12 @@
 
 
 import React from 'react';
+import GreetingContainer from './greeting/greeting_container';
 
 const App = ({ children }) => (
   <div>
-    <h1>Bench BnB</h1>
+    <h1>MangaReads</h1>
+    <GreetingContainer />
     {children}
   </div>
 );
