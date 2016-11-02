2797e6e4336f5a9acdf6ec7d673ecc541fba6185
Christopher <Button@ChristophersAir.att.net>
2016-11-01 22:19:43 -0700

    Continue from bootstrapping auth

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,19 @@
 
 
-
 import React from 'react';
 import { Provider } from 'react-redux';
+import App from './app';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import SessionFormContainer from './session_form/session_form_container';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    // Router goes here...
+    <Router history={hashHistory}>
+      <Route path ="/" component={App} />
+        <Route path="/login" component={SessionFormContainer} />
+        <Route path="/signup" component={SessionFormContainer} />
+    </Router>
   </Provider>
 );
+
+export default Root;
--- frontend/mangareads.jsx
+++ frontend/mangareads.jsx
@@ -2,12 +2,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login} from './util/session_api_util';
+import Root from './components/root';
 import configureStore from './store/store';
 
+import { login } from './actions/session_actions';
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>こんにちは</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
+    window.root = Root;
+    window.store = store;
     window.login = login;
-    window.store = configureStore();
 });
