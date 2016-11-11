62d12e582b32eb5509103cf667b5498af2c051ea
CJButton <gsyme1907@gmail.com>
2016-11-10 16:17:40 -0800

    trying to get checkboxes to update

--- frontend/components/manga/manga.jsx
+++ frontend/components/manga/manga.jsx
@@ -6,42 +6,53 @@ import React from 'react';
 class MangaShow extends React.Component{
   constructor(props) {
     super(props);
-
-    this.state = {
-      isChecked: false
-
-    };
+    // this.bookshelfIds = this.props.bookshelves.map((shelf) => {
+    //     return shelf.id;
+    //   });
+    this.isChecked = this.isChecked.bind(this);
   }
-
-
-  toggleCheckbox: function () {
-  this.setState({
-    isChecked: ! this.state.isChecked
-  });
-
-  this.props.handleCheckboxChange(this.props.label);
-},
 
 
   toggle(shelfId, mangaId) {
     this.props.toggleShelf(shelfId, mangaId);
   }
 
+  // componentWillReceiveProps(newProps) {
+  //   let checkboxes = {};
+  //
+  //   newProps.bookshelves.forEach((shelf) => {
+  //     checkboxes[shelf.id] = this.isChecked(shelf.id);
+  //   });
+  //   this.setState(checkboxes);
+  // }
+
   componentDidMount() {
     this.props.requestAllBookshelves();
   }
 
-  updateSelection(property) {
-    return e => this.setState({[property]: e.target.value});
-  }
+  isChecked(myShelfId) {
+    // console.log(myShelfId);
 
+    this.bookshelfIds = this.props.bookshelves.map((shelf) => {
+        return shelf.id;
+
+      });
+    let checked = "false";
+    this.bookshelfIds.forEach((id) => {
+      if (id === myShelfId) {
+        checked = "true";
+      }
+    });
+    return checked;
+  }
 
   render() {
 
     return (
     <div className="single-manga-show">
       <div className="single-manga-picture">
-        <li><img className="manga-picture-show" src={this.props.manga.img_url} width="210" height="300"/></li>
+        <li><img className="manga-picture-show"
+                 src={this.props.manga.img_url} width="210" height="300"/></li>
 
       </div>
       <ul className="single-manga-words">
@@ -58,6 +69,8 @@ class MangaShow extends React.Component{
         </select>
 
           {this.props.bookshelves.map((shelf, index) => {
+
+            console.log(this.isChecked(shelf.id));
             return (
               <li key={shelf.id}>
                 <label>
@@ -66,7 +79,7 @@ class MangaShow extends React.Component{
                     type="checkbox"
                     onClick={this.toggle.bind(this, shelf.id, this.props.manga.id)}
                     value={index}
-                    checked={this.state.isChecked}/>
+                    defaultChecked={this.isChecked(shelf.id)}/>
                   {shelf.title}
                 </label>
               </li>
