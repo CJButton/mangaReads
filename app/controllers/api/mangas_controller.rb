28c0f2ebfc97283155c2e4b4954afa1bd478fc48
Christopher <Button@Christophers-MacBook-Air.local>
2016-11-15 19:58:47 -0800

    get the index page to render properly

--- app/controllers/api/mangas_controller.rb
+++ app/controllers/api/mangas_controller.rb
@@ -4,12 +4,6 @@
 class Api::MangasController < ApplicationController
 
 
-  # show all manga?
-  # Do I really need this? Leave it for now. It might be useful later.
-  # def index
-    # @manga = Manga.all
-  # end
-
   def index
     if params[:filter].nil?
       @manga = Manga.all
