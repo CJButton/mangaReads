aa0cfe6bd27c8222ddb7dc5b5108e4eb00bbd4b2
CJButton <gsyme1907@gmail.com>
2016-12-16 14:31:16 -0800

    almost have personal shelves highlighting corretly

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -11,15 +11,26 @@ class Sidebar extends React.Component{
     this.state = {
       bookshelves: this.props.bookshelves,
       shelfname: "",
-      currently: "bg",
-      read: "bg",
-      want: "bg",
+      Currently: "bg",
+      Read: "bg",
+      Want: "bg",
       all: "highlight"
     };
 
     this.handleAll = this.handleAll.bind(this);
     this.handleShelf = this.handleShelf.bind(this);
     this.handleAddShelf = this.handleAddShelf.bind(this);
+  }
+
+  componentWillReceiveProps(nextprops) {
+    console.log(this.state);
+    if (nextprops.bookshelves.length > 0) {
+      nextprops.bookshelves.map((shelf) => {
+        this.setState({
+          [shelf.title]: "bg"
+        });
+      });
+    }
   }
 
   update(property) {
@@ -32,6 +43,8 @@ class Sidebar extends React.Component{
 
   handleShelf(e) {
     let target = e.currentTarget.innerHTML;
+    // console.log(target);
+    // console.log(this.state[target]);
     this.props.requestAllManga(target);
     this.setState({
       currently: "bg",
@@ -50,20 +63,36 @@ class Sidebar extends React.Component{
 
   getComicsForShelf(shelfname) {
     this.props.requestAllManga(shelfname);
-    let name = shelfname.split("-");
+    this.setStatusShelvesToBG();
 
+    let name = shelfname.split("-");
+    this.setNewHighlight(name[0].toLowerCase());
+  }
+
+  setStatusShelvesToBG() {
     this.setState({
       currently: "bg",
       read: "bg",
       want: "bg",
       all: "bg"
     });
-    this.setNewHighlight(name[0].toLowerCase());
   }
 
-  setNewHighlight(status) {
+  getComicsForPersonalShelf(shelfname) {
+    this.props.requestAllManga(shelfname);
+
+    this.setStatusShelvesToBG();
+    this.props.bookshelves.map((shelf) => {
+      this.setState({
+        [shelf.title]: "bg"
+      });
+    });
+    this.setNewHighlight(shelfname);
+  }
+
+  setNewHighlight(shelf) {
     this.setState({
-      [status]: "highlight"
+      [shelf]: "highlight"
     });
   }
 
@@ -73,6 +102,7 @@ class Sidebar extends React.Component{
   // try iterating through the bookshelf array with each one of the names
   // to turn them off; then turn on the correct one
   render() {
+    console.log(this.state);
     return (
       <main className="sidebar-container">
         <div className="sidbar-wrapper">
@@ -100,7 +130,8 @@ class Sidebar extends React.Component{
           <li className="shelf-title">Bookshelves</li>
           {this.props.bookshelves.map((shelf, i) => {
             return (<div key={i} className="sidebar-generated-buttons">
-            <div className={`sidebar-shelves button ` + this.state.title + '' + this.state} onClick={this.handleShelf}>{shelf.title}</div>
+            <div className={`sidebar-shelves button ` + this.state[shelf.title] }
+                    onClick={this.getComicsForPersonalShelf.bind(this, shelf.title)}>{shelf.title}</div>
             <div className="sidebar-delete button" onClick={this.props.deleteBookshelf.bind(this, shelf.id)}>X</div>
 
             <br></br>
