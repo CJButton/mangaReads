be2afc37d3b1db0024fb18040d2d261c074ee2e3
CJButton <gsyme1907@gmail.com>
2016-12-20 12:35:59 -0600

    trying to hide/show form when needed

--- frontend/components/manga/reviews.jsx
+++ frontend/components/manga/reviews.jsx
@@ -15,16 +15,16 @@ class Reviews extends React.Component {
     this.handleTitle = this.handleTitle.bind(this);
     this.handleText = this.handleText.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.displayFormOrReview = this.displayFormOrReview.bind(this);
   }
 
   // collect all the data here before sending it off to be added to db
   handleSubmit(e) {
     e.preventDefault();
-    console.log(this.props);
     this.props.submit(this.props.user.id, this.props.manga.id,
              this.state.rating, this.state.title, this.state.text);
-
   }
+
 
   onStarClick(nextValue, prevValue, name) {
     this.setState({rating: nextValue});
@@ -38,37 +38,58 @@ class Reviews extends React.Component {
     this.setState({text: e.target.value});
   }
 
+  displayForm() {
+    console.log("in displayForm");
+    return(
+      <div className="reviewForm">
+        <div className="formTop">
+          <div className="formTopLeft">
+            <p>Username: {this.props.user.username}</p>
+            <p>Manga Title: {this.props.manga.title}</p>
+          </div>
+          <div className="formTopRight">
+            <StarRatingComponent
+              className="starRating"
+              name="rater"
+              starCount={5}
+              value={this.state.rating}
+              onStarClick={this.onStarClick.bind(this)}/>
+          </div>
+        </div>
+        <form className="formDocument"
+              onSubmit={this.handleSubmit}>
+          <input className="review-text"
+                 type="text"
+                 onChange={this.handleTitle}></input>
+          <input className="review-textarea"
+                 type="textarea"
+                  onChange={this.handleText}></input>
+          <input className="review-submit"
+                 type="submit"></input>
+        </form>
+      </div>
+    );
+  }
+
+  displayFormOrReview() {
+
+    if (this.props.reviews.length > 0) {
+      this.props.reviews.forEach((review) => {
+        return(
+          <p>If they have already created a review, display this.</p>
+        );
+      });
+    } else {
+      console.log("review length is null");
+      this.displayForm();
+    }
+  }
+
   render() {
+    console.log(this.props);
     return(
       <div className="reviews">
-        <div className="reviewForm">
-          <div className="formTop">
-            <div className="formTopLeft">
-              <p>Username: {this.props.user.username}</p>
-              <p>Manga Title: {this.props.manga.title}</p>
-            </div>
-            <div className="formTopRight">
-              <StarRatingComponent
-                className="starRating"
-                name="rater"
-                starCount={5}
-                value={this.state.rating}
-                onStarClick={this.onStarClick.bind(this)}/>
-            </div>
-          </div>
-          <form className="formDocument"
-                onSubmit={this.handleSubmit}>
-            <input className="review-text"
-                   type="text"
-                   onChange={this.handleTitle}></input>
-            <input className="review-textarea"
-                   type="textarea"
-                    onChange={this.handleText}></input>
-            <input className="review-submit"
-                   type="submit"></input>
-          </form>
-        </div>
-
+        {this.displayFormOrReview()}
       </div>
     );
   }
