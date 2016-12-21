8adb6f1eeb2948149a3b16f97e364d9b576e9f6f
CJButton <gsyme1907@gmail.com>
2016-12-21 16:36:08 -0600

    fixed error: didn't have enough params in middleware (needed one for username)

--- frontend/middleware/review_middleware.js
+++ frontend/middleware/review_middleware.js
@@ -26,7 +26,7 @@ const ReviewMiddleware = ({ getState, dispatch }) => next => action => {
     case SUBMIT_REVIEW:
       success = (reviews) => dispatch(receiveReview(reviews));
       sendReview(action.userId, action.mangaId, action.rating,
-              action.title, action.description, success, errorCallBack);
+              action.title, action.description, action.username, success, errorCallBack);
       return next(action);
 
     default:
--- frontend/reducers/review_reducer.js
+++ frontend/reducers/review_reducer.js
@@ -9,13 +9,14 @@ import { merge } from 'lodash';
 
 const ReviewReducer = (state = {}, action) => {
   console.log(action);
+
   switch(action.type) {
     case RECEIVE_MANGA_REVIEWS:
       const newReviews = merge({}, action.reviews);
         return merge(newReviews);
 
     case RECEIVE_REVIEW:
-        return merge({}, state, action.reviews);
+        return merge({}, state, action.review);
 
     default:
         return state;
