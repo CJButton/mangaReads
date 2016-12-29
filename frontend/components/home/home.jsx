14055290f9fa49634bf998799cb12307e0d3532a
CJButton <gsyme1907@gmail.com>
2016-12-29 05:01:18 -0600

    homepage is basically finished; still need a couple of touches

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -6,31 +6,61 @@ const Home = ( {manga} ) => (
   <main className="home-container">
     <div className="home-left">
     </div>
-    <div className="home-center">
-      <ul>
+    <ul>
       <li className="home-title">Welcome to mangaReads!</li>
       <br></br>
       <li className="home-description">Click on a manga below to get more infromation about it.</li>
       <li className="home-greeting">We hope you enjoy and discover a little more about Japan by reading these comics!</li>
-      </ul>
-      {
-        manga.map((comic, i) => (
-      <ul key={i} className="mangaHomeDisplay">
-          <a href={`#/manga/${comic.id}`}>
-            <img className="homePicture" src={comic.img_url} />
-          </a>
-          <ul className="homeMangaText">
-          </ul>
-      </ul>
-      ))
-     }
-    </div>
-    <ul>
-
     </ul>
+    <div className="home-center">
+
+
+
+      {manga.map((comic, i) => {
+        return(
+          <div className="library">
+          <div className="comicWrapper">
+            <a href={`#/manga/${comic.id}`}>
+              <img className="homePicture" src={comic.img_url} />
+            </a>
+          </div>
+        </div>
+        );
+      })}
+      <br></br>
+      <br></br>
+    </div>
+
     <div className="home-right">
     </div>
   </main>
 );
 
 export default Home;
+
+// <div className="home-left">
+// </div>
+// <div className="home-center">
+//   <ul>
+//   <li className="home-title">Welcome to mangaReads!</li>
+//   <br></br>
+//   <li className="home-description">Click on a manga below to get more infromation about it.</li>
+//   <li className="home-greeting">We hope you enjoy and discover a little more about Japan by reading these comics!</li>
+//   </ul>
+//   {
+//     manga.map((comic, i) => (
+//   <ul key={i} className="mangaHomeDisplay">
+//       <a href={`#/manga/${comic.id}`}>
+//         <img className="homePicture" src={comic.img_url} />
+//       </a>
+//       <ul className="homeMangaText">
+//       </ul>
+//   </ul>
+//   ))
+//  }
+// </div>
+// <ul>
+//
+// </ul>
+// <div className="home-right">
+// </div>
