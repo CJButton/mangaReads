025f9dfd28f225c364b0680146d3ee78b37a2def
CJButton <gsyme1907@gmail.com>
2017-08-26 12:24:15 +0900

    carousel images are onw hidden in bg

--- frontend/components/splash/splash_component.jsx
+++ frontend/components/splash/splash_component.jsx
@@ -7,8 +7,8 @@ import {
   Row,
   Col,
   Image,
+  Carousel
 } from 'react-bootstrap';
-
 
 class SplashComponent extends React.Component{
   constructor(props) {
@@ -65,7 +65,6 @@ class SplashComponent extends React.Component{
 	}
 
   renderErrors() {
-
     if (this.props.errors.length > 0) {
       return(
           <ul>
@@ -80,8 +79,6 @@ class SplashComponent extends React.Component{
     else {
       return (
         <div className="splash-errorsBG-hidden">
-
-
         </div>
       )
   }
@@ -91,12 +88,37 @@ class SplashComponent extends React.Component{
     this.redirectIfLoggedIn();
   }
 
+  // <Carousel.Item>
+  //    <img width={900} height={500} src="http://res.cloudinary.com/ddbfkqb9m/image/upload/q_30/v1493502077/cropped-1920-1200-451608_v8lzqm.jpg"/>
+  // </Carousel.Item>
+  // <Carousel.Item>
+  //    <img width={900} height={500} src="http://res.cloudinary.com/ddbfkqb9m/image/upload/q_30/v1493502076/cropped-1920-1200-109375_bmkmet.jpg"/>
+  // </Carousel.Item>
+  // <Carousel.Item>
+  //    <img width={900} height={500} src="http://res.cloudinary.com/ddbfkqb9m/image/upload/q_30/v1493502078/cropped-1920-1200-474477_a2qnlp.jpg"/>
+  // </Carousel.Item>
 
   render() {
     const {username, password1, name, email, password2} = this.state;
 
     return (
     <nav className="login-signup">
+
+      <Carousel
+          className="hero-carousel-wrapper"
+          indicators={false}
+          controls={false}>
+          <Carousel.Item
+             className='hero-carousel-img'>
+             <img src="http://res.cloudinary.com/ddbfkqb9m/image/upload/q_30/v1493502079/cropped-1920-1200-741091_yudxc9.jpg"/>
+             <Carousel.Caption
+               className='hero-carousel-caption'>
+               <p>
+                 One Punch Man
+               </p>
+             </Carousel.Caption>
+          </Carousel.Item>
+      </Carousel>
       {/*
         <div className="captioned-gallery">
           <figure className="slider">
@@ -123,98 +145,97 @@ class SplashComponent extends React.Component{
           </figure>
         </div>
         */}
+          <div className="splash-top-bar">
+            <Col xs={8} xsOffset={2} sm={5} smOffset={1} md={4} mdOffset={1}>
+              <img className="logo" src="http://res.cloudinary.com/ddbfkqb9m/image/upload/v1478400202/Extras/Logomakr_6eQJJ4.png"/>
+            </Col>
 
-        <div className="splash-top-bar">
-          <Col xs={8} xsOffset={2} sm={5} smOffset={1} md={4} mdOffset={1}>
-            <img className="logo" src="http://res.cloudinary.com/ddbfkqb9m/image/upload/v1478400202/Extras/Logomakr_6eQJJ4.png"/>
+            <Col xs={8} xsOffset={2} sm={4} smOffset={1} md={5} mdOffset={1}>
+              <div className="login-form-wrapper">
+                <form className="loginForm" onSubmit={this.handleLogin}>
+                  <div className="splash-login-wrapper">
+                    <input type="text"
+                      placeholder="Username"
+                      value={username}
+                      onChange={this.update("username")}/>
+
+                    <input type="password"
+                      placeholder="Password"
+                      value={password1}
+                      onChange={this.update("password1")}/>
+                  </div>
+
+                  <div className="loginButtons">
+
+                    <input className="auth-login-button button"
+                      type="submit" value="Login"/>
+                    &nbsp;
+
+                  </div>
+                </form>
+              </div>
+            </Col>
+          </div>
+
+          <Col xs={8} xsOffset={2} sm={4} smOffset={7} md={3} mdOffset={6}
+            className="errorsContainer">
+            {this.renderErrors()}
           </Col>
 
-          <Col xs={8} xsOffset={2} sm={4} smOffset={1} md={5} mdOffset={1}>
-            <div className="login-form-wrapper">
-              <form className="loginForm" onSubmit={this.handleLogin}>
-                <div className="splash-login-wrapper">
-                  <input type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={this.update("username")}/>
-
-                  <input type="password"
-                    placeholder="Password"
-                    value={password1}
-                    onChange={this.update("password1")}/>
-                </div>
-
-                <div className="loginButtons">
-
-                  <input className="auth-login-button button"
-                    type="submit" value="Login"/>
-                  &nbsp;
-
-                </div>
-              </form>
-            </div>
+          <Col xs={10} xsOffset={1} md={6} mdOffset={1}
+            className="mottoContainer">
+            <h3 className="mottoSplash">
+              There is more to Japan than sushi and samurai.
+            </h3>
+            <div className="mottoSplashbottom">There's also comic books!</div>
+            <p className="mottoSubSplash">
+              Action, adventure, romance, a slice-of-life, and more! You'll
+              find it all in these comics from Japan. Organize your collection,
+              and find new comics to enjoy, here with mangaReads!
+            </p>
           </Col>
-        </div>
 
-        <Col xs={8} xsOffset={2} sm={4} smOffset={7} md={3} mdOffset={6}
-             className="errorsContainer">
-          {this.renderErrors()}
-        </Col>
+          <Col xs={8} xsOffset={2} sm={4} smOffset={4} md={3} mdOffset={1}>
+            <form className="signUpForm" onSubmit={this.handleSignup}>
+              <h4>New here? Create a free account!</h4>
 
-        <Col xs={10} xsOffset={1} md={6} mdOffset={1}
-             className="mottoContainer">
-          <h3 className="mottoSplash">
-            There is more to Japan than sushi and samurai.
-          </h3>
-          <div className="mottoSplashbottom">There's also comic books!</div>
-          <p className="mottoSubSplash">
-            Action, adventure, romance, a slice-of-life, and more! You'll
-            find it all in these comics from Japan. Organize your collection,
-            and find new comics to enjoy, here with mangaReads!
-          </p>
-        </Col>
+              <input type="text"
+                placeholder="Name"
+                value={name}
+                onChange={this.update("name")} />
 
-        <Col xs={8} xsOffset={2} sm={4} smOffset={4} md={3} mdOffset={1}>
-          <form className="signUpForm" onSubmit={this.handleSignup}>
-            <h4>New here? Create a free account!</h4>
+              <input type="text"
+                placeholder="Email Address"
+                value={email}
+                onChange={this.update("email")} />
 
-            <input type="text"
-              placeholder="Name"
-              value={name}
-              onChange={this.update("name")} />
+              <input type="password"
+                placeholder="Password"
+                value={password2}
+                onChange={this.update("password2")}/>
 
-            <input type="text"
-              placeholder="Email Address"
-              value={email}
-              onChange={this.update("email")} />
+              <button className="submitButton button splashButtons"
+                type="submit"
+                placeholder="Sign up">
+                Sign Up</button>
 
-            <input type="password"
-              placeholder="Password"
-              value={password2}
-              onChange={this.update("password2")}/>
+              <button className="guestLogin button splashButtons" type="submit"
+                onClick={this.handleGuest}
+                value="Guest Login">Guest Login</button>
 
-            <button className="submitButton button splashButtons"
-              type="submit"
-              placeholder="Sign up">
-              Sign Up</button>
-
-            <button className="guestLogin button splashButtons" type="submit"
-              onClick={this.handleGuest}
-              value="Guest Login">Guest Login</button>
-
-          </form>
-        </Col>
+            </form>
+          </Col>
 
 
-        <footer className="splash-footer">
-          <a href="https://www.linkedin.com/in/cjbutton" target="_blank">
-            <img src="http://res.cloudinary.com/ddbfkqb9m/image/upload/v1493527968/In-2CRev-14px_npyzzk.png"/>
-          </a>
+          <footer className="splash-footer">
+            <a href="https://www.linkedin.com/in/cjbutton" target="_blank">
+              <img src="http://res.cloudinary.com/ddbfkqb9m/image/upload/v1493527968/In-2CRev-14px_npyzzk.png"/>
+            </a>
 
-          <a href="https://github.com/CJButton" target="_blank">
-            <img src="http://res.cloudinary.com/ddbfkqb9m/image/upload/v1493520070/GitHub_Logo_k7corb.png"/>
-          </a>
-        </footer>
+            <a href="https://github.com/CJButton" target="_blank">
+              <img src="http://res.cloudinary.com/ddbfkqb9m/image/upload/v1493520070/GitHub_Logo_k7corb.png"/>
+            </a>
+          </footer>
     </nav>
     );
   }
