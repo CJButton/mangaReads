05f5c92b46fc54fed1e2ebcf4637b024020a8fc8
CJButton <gsyme1907@gmail.com>
2017-09-21 11:34:28 +0900

    added genres table and assocaitions with comcis

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -4,12 +4,13 @@
 class Manga < ActiveRecord::Base
   validates :title, :author, :synopsis, presence: true
   validates :title, uniqueness: true
-  
+
   attr_accessor :avg
 
   has_many :mangabookshelves, :class_name => 'MangaBookshelf'
   has_many :bookshelves, through: :mangabookshelves, source: :bookshelf
   has_many :reviews
+  has_many :genres
 
 
   def self.filter(current_user, shelf_name)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231044855) do
+ActiveRecord::Schema.define(version: 20170921020543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20161231044855) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "genre",      null: false
+    t.integer  "manga_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "genres", ["manga_id"], name: "index_genres_on_manga_id", using: :btree
 
   create_table "manga_bookshelves", force: :cascade do |t|
     t.integer  "manga_id",     null: false
