71ad4fb0df085d9b75b425f4b3e34b1fe7eab3b8
CJButton <gsyme1907@gmail.com>
2017-09-29 19:28:23 +0900

    home page can now detect which comic the mouse has hovered over

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -10,39 +10,56 @@ import { Grid,
          Image } from 'react-bootstrap';
 
 import TopBarContainer from '../topbar/topbar_container';
+class Home extends React.Component{
+  constructor(props) {
+    super(props);
 
-const Home = ( props ) => {
+    this.comicHoverEnter = this.comicHoverEnter.bind(this);
+  }
+
+  comicHoverEnter(id) {
+    console.log(`entered comic ${id}`);
+  }
+
    //background-color of #root chnged to grey to help with nighttime
    ///
-  const actions = props.manga.action;
-  const romances = props.manga.romance;
+ render() {
+  const actions = this.props.manga.action;
+  const romances = this.props.manga.romance;
     return (
-  <div>
-    <TopBarContainer />
-    <h3>
-      Action
-    </h3>
-  {actions &&
-    <div className='slider-wrapper'>
-      <Slider actions={actions} isInfinite delay={1000000}>
-        {actions.map((comic, i) => <div key={i}><img src={comic.img_url} /></div>)}
-      </Slider>
-    </div>
+      <div>
+        <TopBarContainer />
+        <h3>
+          Action
+        </h3>
+      {actions &&
+        <div className='slider-wrapper'>
+          <Slider actions={actions} delay={1000000}>
+            {actions.map((comic, i) =>
+              <div key={i} className='slider-img'>
+                <img
+                  onMouseEnter={() => this.comicHoverEnter(i)}
+                  onMouseLeave={this.someOtherHandler}
+                  src={comic.img_url} />
+              </div>)}
+          </Slider>
+        </div>
+      }
+      <br />
+      <br />
+      <h3>
+        Romance
+      </h3>
+      {romances &&
+        <div className='slider-wrapper'>
+          <Slider romances={romances} delay={1000000}>
+            {romances.map((comic, i) => <div key={i}><img src={comic.img_url} /></div>)}
+          </Slider>
+        </div>
+      }
+      </div>
+      );
   }
-  <br />
-  <br />
-  <h3>
-    Romance
-  </h3>
-  {romances &&
-    <div className='slider-wrapper'>
-      <Slider romances={romances} isInfinite delay={1000000}>
-        {romances.map((comic, i) => <div key={i}><img src={comic.img_url} /></div>)}
-      </Slider>
-    </div>
-  }
-  </div>
-  );
 };
 
 export default Home;
